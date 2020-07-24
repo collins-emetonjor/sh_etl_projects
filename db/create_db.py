e08ffa2265fcf311df646fc2f2f750bdf221d4b2
collins emetonjor <collins_emetonjor@live.co.uk>
2020-07-24 12:44:54 +0100

    Added script to create db

---
+++ db/create_db.py
@@ -0,0 +1,7 @@
+"""
+Author: Collins Emetonjor       Date: 24/07/2020     Comment: script to create 'sh_dev' sqllite db to hold sample data
+
+Change History
+Author:                         Date:                Comment
+
+"""
